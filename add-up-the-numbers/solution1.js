252d299582df1407fdf63254ed5f1a4ebf55beea
enteryana <valchenkoyana@gmail.com>
2024-04-11 13:42:14 +0200

    add changes

---
+++ add-up-the-numbers/solution1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/*
+
+## calcAge Function
+
+### Description
+The `calcAge` function calculates the approximate number of days lived based on the given age.
+
+### Syntax
+```javascript
+function calcAge(age)
+ 
+ */
+
+function calcAge(age) {
+    const daysInYear = 365;
+    return age * daysInYear;
+}
